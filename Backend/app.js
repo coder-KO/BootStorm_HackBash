6e37a011a5bc6433bbcb65c4947d1f6ea3cd5d80
AAA530 <dhairyapatelad@gmail.com>
2021-03-13 12:31:35 +0530

    added app.js

---
+++ Backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const UserRoutes = require("./routes/usersRoutes");
+require("dotenv").config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.set("useNewUrlParser", true);
+mongoose.set("useFindAndModify", false);
+mongoose.set("useCreateIndex", true);
+mongoose.set("useUnifiedTopology", true);
+
+mongoose.connect("mongodb://localhost/Docu_Verify", (err, db) => {
+  console.log("Db connected");
+});
+
+app.use("/users", UserRoutes);
+
+const port = 5000 || process.env.PORT;
+app.listen(port, () => {
+  console.log(`Server is started on port :${port}`);
+});
