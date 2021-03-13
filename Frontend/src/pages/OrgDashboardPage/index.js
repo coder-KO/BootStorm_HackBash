21380f88fa3b507ce998717b672150c186b81832
AAA530 <dhairyapatelad@gmail.com>
2021-03-14 01:42:49 +0530

    adding route auths

--- Frontend/src/pages/OrgDashboardPage/index.js
+++ Frontend/src/pages/OrgDashboardPage/index.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useEffect, useContext, useState, useHistory } from "react";
 import OrgDashboard from "../../components/OrgDashboard";
+import { UserContext } from "./../../context/UserContext";
 
 const OrgDashboardPage = () => {
+  const { userData } = useContext(UserContext); //getting Data form UserContext
+  const [loading, setLoading] = useState(true);
+  const history = useHistory();
+
+  //Checking if User is Logged in , if not then send him to login page
+  useEffect(() => {
+    console.log(userData);
+    if (!userData.user) history.push("/home");
+    setLoading(false);
+  });
   return (
     <>
       <OrgDashboard />
