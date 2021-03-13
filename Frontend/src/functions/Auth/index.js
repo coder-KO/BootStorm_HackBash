e7ec3fbdbfdc0542bf689ce9131c35a8389ff0d2
AAA530 <dhairyapatelad@gmail.com>
2021-03-14 00:46:47 +0530

    added helper in axios

--- Frontend/src/functions/Auth/index.js
+++ Frontend/src/functions/Auth/index.js
@@ -1,42 +1,30 @@
-import axios from "axios";
+// import axios from "axios";
+import axios from "./../../helper/axios";
 
+const Login = async (userData) => {
+  try {
+    console.log(userData);
+    //calling the register API
+    const returnData = await axios.post("/login", userData);
+    console.log(returnData);
+    const token = returnData.data.token;
+    console.log(token);
 
-const Login = async(userData) =>  {
+    // varifying the token
 
-    try{
-       
-        console.log(userData);
-        //calling the register API
-        const returnData = await axios.post("http://localhost:5000/users/login", userData);
-        const token = returnData.token;
-        console.log(token)
+    const isValid = await axios.post("/tokenIsValid");
 
-        // varifying the token
-
-        const isValid = await axios.post("http://localhost:5000/users/tokenIsValid", null, {
-            headers:{
-                'Authorization': `Bearer ${token}`
-            }
-        });
-
-        if(isValid === true){
-
-            //re-route to the dashboard page
-            console.log("I'm In")
-        }
-
-        if(isValid === false){
-            console.log("Unauthorized");
-        }
-
-        
-
-    }catch(err){
-        console.log(err)
+    if (isValid === true) {
+      //re-route to the dashboard page
+      console.log("I'm In");
     }
 
-}
+    if (isValid === false) {
+      console.log("Unauthorized");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-export {
-    Login
-}
+export { Login };
