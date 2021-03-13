2bcf8c6ab85b6b9d39bdf51612808da522e06942
Krishna <drkaojha@gmail.com>
2021-03-14 01:43:24 +0530

    adding upload component

--- Frontend/src/components/OrgDashboard/Upload.js
+++ Frontend/src/components/OrgDashboard/Upload.js
@@ -1,5 +1,103 @@
-import React from "react";
+import React, { useMemo } from "react";
+import { Grid, Typography, Button, TextField } from "@material-ui/core";
+import { useDropzone } from "react-dropzone";
 
-export default function Upload() {
-  return <div>Upload File Section will go here</div>;
+const baseStyle = {
+  flex: 1,
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  padding: "20px",
+  borderWidth: 2,
+  borderRadius: 2,
+  borderColor: "#eeeeee",
+  borderStyle: "dashed",
+  backgroundColor: "#fafafa",
+  color: "#bdbdbd",
+  outline: "none",
+  transition: "border .24s ease-in-out",
+};
+
+const activeStyle = {
+  borderColor: "#2196f3",
+};
+
+const acceptStyle = {
+  borderColor: "#00e676",
+};
+
+const rejectStyle = {
+  borderColor: "#ff1744",
+};
+
+export default function Upload(props) {
+  const {
+    getRootProps,
+    getInputProps,
+    isDragActive,
+    isDragAccept,
+    isDragReject,
+  } = useDropzone({ accept: "image/*" });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isDragActive, isDragReject, isDragAccept]
+  );
+
+  return (
+    <div className="container">
+      <Grid container spacing={4}>
+        <Grid item xs={12}>
+          <div {...getRootProps({ style })}>
+            <input {...getInputProps()} />
+            <p>
+              Drag 'n' drop some files here, or click to select files to verify
+            </p>
+          </div>
+        </Grid>
+        <Grid
+          itex
+          xs={6}
+          style={{
+            display: "flex",
+            justifyContent: "flex-start",
+            alignItems: "center",
+            padding: 16,
+          }}
+        >
+          <TextField
+            id="verifiedBy"
+            size="small"
+            readOnly
+            value="Verified by"
+            variant="filled"
+            InputProps={{ style: { borderColor: "red", color: "#008891" } }}
+            fullWidth
+          />
+        </Grid>
+        <Grid
+          itex
+          xs={6}
+          style={{
+            display: "flex",
+            justifyContent: "flex-end",
+            alignItems: "center",
+            padding: 16,
+          }}
+        >
+          <Button
+            variant="contained"
+            style={{ background: "#008891", color: "#fff" }}
+          >
+            Verify
+          </Button>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
